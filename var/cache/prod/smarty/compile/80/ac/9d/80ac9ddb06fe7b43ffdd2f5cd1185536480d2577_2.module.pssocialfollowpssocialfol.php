ab3c010250edc882f46c476d9cce210569029210
giorgi <giorgi.bibilashvili89@gmail.com>
2018-09-12 08:41:06 +0400

    --

---
+++ var/cache/prod/smarty/compile/80/ac/9d/80ac9ddb06fe7b43ffdd2f5cd1185536480d2577_2.module.pssocialfollowpssocialfol.php
@@ -0,0 +1,66 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-09-12 08:40:09
+  from 'module:pssocialfollowpssocialfol' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5b9898a9aabe33_31684032',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '80ac9ddb06fe7b43ffdd2f5cd1185536480d2577' => 
+    array (
+      0 => 'module:pssocialfollowpssocialfol',
+      1 => 1536603732,
+      2 => 'module',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b9898a9aabe33_31684032 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_10291174665b9898a9aa3286_96326175', 'block_social');
+?>
+
+<?php }
+/* {block 'block_social'} */
+class Block_10291174665b9898a9aa3286_96326175 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'block_social' => 
+  array (
+    0 => 'Block_10291174665b9898a9aa3286_96326175',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+  <div class="block-social col-lg-4 col-md-12 col-sm-12">
+    <ul>
+      <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['social_links']->value, 'social_link');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['social_link']->value) {
+?>
+        <li class="<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['social_link']->value['class'], ENT_QUOTES, 'UTF-8');?>
+"><a href="<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['social_link']->value['url'], ENT_QUOTES, 'UTF-8');?>
+" target="_blank"><?php echo htmlspecialchars($_smarty_tpl->tpl_vars['social_link']->value['label'], ENT_QUOTES, 'UTF-8');?>
+</a></li>
+      <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+    </ul>
+  </div>
+<?php
+}
+}
+/* {/block 'block_social'} */
+}
